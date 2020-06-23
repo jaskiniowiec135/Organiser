5ca67a88d7ac5af444abeceb41f4e6ec94654f37
Fersus4502 <38700360+Fersus4502@users.noreply.github.com>
2020-06-23 19:41:56 +0200

    frontend basic interface

--- Organiser/dev/Organiser.Droid/MainActivity.cs
+++ Organiser/dev/Organiser.Droid/MainActivity.cs
@@ -11,15 +11,42 @@ namespace Organiser.Droid
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        bool on_start_page;
         protected override void OnCreate(Bundle bundle)
         {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
-
+            //TabLayoutResource = Resource.Layout.Tabbar;
+            //ToolbarResource = Resource.Layout.Toolbar;
+            on_start_page = true;
             base.OnCreate(bundle);
             global::Xamarin.Forms.Forms.Init(this, bundle);
+            SetContentView(Resource.Layout.Toolbar);
+            var toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
 
             LoadApplication(new App());
+        }
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.navigation, menu);
+            return base.OnCreateOptionsMenu(menu);
+        }
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            if (item.ItemId == Resource.Id.add_activity)
+            {
+                SetContentView(Resource.Layout.Add_Activity);
+                on_start_page = false;
+            }
+            return base.OnOptionsItemSelected(item);
+        }
+        public override void OnBackPressed()
+        {
+            if (on_start_page == false)
+            {
+                SetContentView(Resource.Layout.Toolbar);
+                on_start_page = true;
+            }
+            base.OnBackPressed();
         }
     }
 }
